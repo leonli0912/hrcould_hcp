e416166e0e89729a2680250704d56507655878df
leonli0912 <ly95487532@163.com>
2016-12-20 01:58:31 +0800

    change to object view done

--- WebContent/controller/Object.controller.js
+++ WebContent/controller/Object.controller.js
@@ -7,8 +7,9 @@ sap.ui.define(['jquery.sap.global', 'sap/m/MessageToast',
 	var CController = Controller.extend("ui5TileTrial.controller.Object", {
 		SERVICE_URL: "/sfserver/User",
 		oRouter: null,
+		objectId:null,
 		onInit: function() {
-			var oDataModel = new sap.ui.model.odata.ODataModel("/sfserver");
+			var oDataModel = new sap.ui.model.odata.v2.ODataModel("/sfserver",{useBatch:false});
 			this.getView().setModel(oDataModel);
 			var oViewModel = new sap.ui.model.json.JSONModel({
 					busy: true,
@@ -18,7 +19,21 @@ sap.ui.define(['jquery.sap.global', 'sap/m/MessageToast',
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 		    this.oRouter.getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 		},
+		onNavBack:function(){
+			this.oRouter.navTo("appHome");
+		},
+		onPost: function (oEvent) {
+			var oDataModel  = this.getView().getModel();
+			var sValue = oEvent.getParameter("value");
+			var oEntry = {
+					"userId" : this.objectId,
+					"custom01":sValue
+					};
+			oDataModel.update("/User('" + this.objectId + "')",oEntry
+				);
+		},
 		_onObjectMatched: function(oEvent) {
+			this.objectId = oEvent.getParameter("arguments").objectId;
 			var sObjectPath = "/User('" + oEvent.getParameter("arguments").objectId + "')";
 			this._bindView(sObjectPath);
 		},
